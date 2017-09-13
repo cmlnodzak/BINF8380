985c83f764af276649a2791cde5cbaad08532185
cmlnodzak <cnodzak@noreply.github.com>
2017-09-13 11:34:51 -0400

    Quiz runs interactively, address exiting bug.

--- src/BINF8380_Lab3/AminoQuizzer.java
+++ src/BINF8380_Lab3/AminoQuizzer.java
@@ -1,53 +1,56 @@
 package BINF8380_Lab3;
 import java.util.Random;
-//import java.util.Timer;
+import java.util.Scanner;
+
 public class AminoQuizzer {
 
-public static String[] SHORT_NAMES = { 
-"A","R", "N", "D", "C", "Q", "E", 
-"G",  "H", "I", "L", "K", "M", "F", 
-"P", "S", "T", "W", "Y", "V" 
-};
+	public static String[] SHORT_NAMES = { 
+			"A","R", "N", "D", "C", "Q", "E", 
+			"G",  "H", "I", "L", "K", "M", "F", 
+			"P", "S", "T", "W", "Y", "V" 
+	};
 
-public static String[] FULL_NAMES = {
-"alanine","arginine", "asparagine", "aspartic acid", "cysteine",
-"glutamine",  "glutamic acid","glycine" ,"histidine", "isoleucine",
-"leucine",  "lysine", "methionine", "phenylalanine", "proline", 
-"serine","threonine","tryptophan", "tyrosine", "valine"
-};
+	public static String[] FULL_NAMES = {
+			"alanine","arginine", "asparagine", "aspartic acid", "cysteine",
+			"glutamine",  "glutamic acid","glycine" ,"histidine", "isoleucine",
+			"leucine",  "lysine", "methionine", "phenylalanine", "proline", 
+			"serine","threonine","tryptophan", "tyrosine", "valine"
+	};
 
+	static Scanner getInput = new Scanner(System.in);
 
-public static void main(String[] args){
-	long beginTime = System.currentTimeMillis();
-	long timeElapsed = 0;
-	int score = 0;
-	Random rand = new Random();
-	while (timeElapsed < 0.5*60*1000) {
-	for (int k = 0;k < 20; k++) {
-		int amino = rand.nextInt(20);
-		System.out.println("What is the one-letter abbreviation for: "+FULL_NAMES[amino]);
-		String usrGuess = System.console().readLine().toUpperCase();
-		if( usrGuess.matches(SHORT_NAMES[amino])){
-			score++;
-			System.out.println("Correct! Your current score is: " +score);
-			timeElapsed = (System.currentTimeMillis() - beginTime);
-			float timegone = timeElapsed/1000f;
-			System.out.println(timegone+"seconds have elapsed!");
-		}
-		else {
-			score--;
-			System.out.println("Wrong! Your current score is: " +score);
-			System.out.println("Correct! Your current score is: " +score);
-			timeElapsed = (System.currentTimeMillis() - beginTime);
-			float timegone = timeElapsed/1000f;
-			System.out.println(timegone+"seconds have elapsed!");
-			
-		}
+	public static void main(String[] args){
+		long beginTime = System.currentTimeMillis();
+		long timesUp = beginTime +30*1000;
+		long timeElapsed = 0;
+		int score = 0;
+		Random rand = new Random();
+		while (System.currentTimeMillis()< timesUp) {
+			for (int k = 0;k < 20; k++) {
+				int amino = rand.nextInt(20);
+				System.out.println("What is the one-letter abbreviation for: "+FULL_NAMES[amino]);
+				String usrGuess = getInput.next().toUpperCase();
+				if( usrGuess.matches(SHORT_NAMES[amino])){
+					score++;
+					System.out.println("Correct! Your current score is: " +score);
+					timeElapsed = (System.currentTimeMillis() - beginTime);
+					float timegone = timeElapsed/1000f;
+					System.out.println(timegone+" seconds have elapsed!");
+				}
+				else {
+					score--;
+					System.out.println("Wrong! Your current score is: " +score);
+					timeElapsed += 31000;
+					System.out.println("You lose! Study more and start over!");
+				
+				}
+			}
 		
-	}
 		}
+	getInput.close();
+	System.out.println("..............");
 	System.out.println("Good job! Time is up!");
 	System.out.println("Your final score was: "+score+ "points!");
-}
-}
 
+	}
+}
