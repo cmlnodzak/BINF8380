173d2dd49dffa4c289642f2c6b0f9ddd9b687a05
cmlnodzak <cnodzak@noreply.github.com>
2017-09-13 10:34:42 -0400

    Make interactive amino acids quiz.

--- src/BINF8380_Lab3/AminoQuizzer.java
+++ src/BINF8380_Lab3/AminoQuizzer.java
@@ -16,22 +16,38 @@ public static String[] FULL_NAMES = {
 "serine","threonine","tryptophan", "tyrosine", "valine"
 };
 
-public static long beginTime = System.currentTimeMillis();
 
 public static void main(String[] args){
-	//long timesUp = 3000;
+	long beginTime = System.currentTimeMillis();
 	long timeElapsed = 0;
+	int score = 0;
 	Random rand = new Random();
-	while (timeElapsed <= 3000) {
+	while (timeElapsed < 0.1*60*1000) {
 	for (int k = 0;k < 20; k++) {
 		int amino = rand.nextInt(20);
 		System.out.println("What is the one-letter abbreviation for: "+FULL_NAMES[amino]);
-		timeElapsed = System.currentTimeMillis() - beginTime;
+		String usrGuess = System.console().readLine().toUpperCase();
+		if( usrGuess.matches(SHORT_NAMES[amino])){
+			score++;
+			System.out.println("Correct! Your current score is: " +score);
+			timeElapsed = (System.currentTimeMillis() - beginTime);
+			float timegone = timeElapsed/1000f;
+			System.out.println(timegone+"seconds have elapsed!");
+		}
+		else {
+			score--;
+			System.out.println("Wrong! Your current score is: " +score);
+			System.out.println("Correct! Your current score is: " +score);
+			timeElapsed = (System.currentTimeMillis() - beginTime);
+			float timegone = timeElapsed/1000f;
+			System.out.println(timegone+"seconds have elapsed!");
+			
+		}
+		
 	}
-		//String usrGuess = System.console().readLine().toUpperCase();
-		//if (usrGuess.matches(SHORT_NAMES[amino])) {
-	//		k++;
-	//		System.out.println("Eureka! You have used "+timeElapsed+ "milliseconds");
-			}
-		}		
+		}
+	System.out.println("Good job! Time is up!");
+	System.out.println("Your final score was: "+score+ "points!");
 }
+}
+
