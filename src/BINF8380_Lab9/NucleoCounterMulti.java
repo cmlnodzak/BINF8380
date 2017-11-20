4d39d427b0a75d4a350c34e139f5ed0a3ed2c077
cmlnodzak <cnodzak@noreply.github.com>
2017-11-20 17:26:26 -0500

    Changed to use AtomicIntegers and split into Worker.

--- src/BINF8380_Lab9/NucleoCounterMulti.java
+++ src/BINF8380_Lab9/NucleoCounterMulti.java
@@ -5,64 +5,86 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
 
-
-public class NucleoCounterMulti {
+public class NucleoCounterMulti{
 		private static final int NUM_THREADS = Runtime.getRuntime().availableProcessors() + 1;
-		final Semaphore mySemaphore = new Semaphore(NUM_THREADS);
-		private volatile static int adenosine = 0;
-		private volatile static int thymine = 0;
-		private volatile static int cytosine = 0;
-		private volatile static int guanine = 0;
-		private volatile static int unknown = 0;
-		public NucleoCounterMulti(File[] listofFiles) throws InterruptedException{
-			mySemaphore.acquire();
-			for (File file : listofFiles) {
+		private static AtomicInteger adenosine = new AtomicInteger(0);
+		private static AtomicInteger thymine = new AtomicInteger(0);
+		private static AtomicInteger cytosine = new AtomicInteger(0);
+		private static AtomicInteger guanine = new AtomicInteger(0);
+		private static AtomicInteger unknown = new AtomicInteger(0);
+		private Semaphore semaphore;
+		private File file;
+		
+	 NucleoCounterMulti(File file, Semaphore semaphore) {
+		this.file = file;
+		this.semaphore = semaphore;
+		}	
+		private static class Worker implements Runnable{
+			private Semaphore semaphore;
+			private File file;
+			Worker(NucleoCounterMulti nucleo){
+				file = nucleo.file;
+				semaphore = nucleo.semaphore;
+			}
+			
+		@Override
+		public void run() {
 			try {
-			    if (file.isFile()) {
-			    	try {
-					BufferedReader readfasta = new BufferedReader(new FileReader(file));
-					for (String fasLine = readfasta.readLine(); fasLine != null; fasLine = readfasta.readLine())
-						{
-						if(!fasLine.startsWith(">")) {
-							getBases(fasLine);
-						}
+				BufferedReader readfasta = new BufferedReader(new FileReader(file));
+				for (String fasLine = readfasta.readLine(); fasLine != null; fasLine = readfasta.readLine()) {
+					if(!fasLine.startsWith(">")) {
+						getBases(fasLine);
 					}
-					readfasta.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-			    }
-		} finally {
-		    mySemaphore.release();}
+				}
+				readfasta.close();
+				semaphore.release();
+			} catch (IOException e) {
+				e.printStackTrace();
+				}
 			}
 		}
-		public void getBases(String fas) {
+		private static void getBases(String fas) {
 			for(int i = 0;i < fas.length() ; i++ ) {
 				char base = fas.charAt(i);
 				if(base == 'G'){
-					guanine++;
+					guanine.incrementAndGet();
 				}else if(base == 'C') {
-					cytosine++;
+					cytosine.incrementAndGet();
 				}else if(base == 'T') {
-					thymine++;
+					thymine.incrementAndGet();
 				}else if(base == 'A') {
-					adenosine++;
+					adenosine.incrementAndGet();
 				}else {
-					unknown++;
+					unknown.incrementAndGet();
 				}
 			}
 		}	
-	
+
 public static void main(String[] args) throws InterruptedException {
 	long startTime = System.currentTimeMillis();
-	File[] files = new File("/Users/mitchnodzak/adenomasRelease/fasta").listFiles();
-	new NucleoCounterMulti(files);
-	System.out.println("Count of A: "+adenosine);
-	System.out.println("Count of T: "+thymine);
-	System.out.println("Count of C: "+cytosine);
-	System.out.println("Count of G: "+guanine);
-	System.out.println("Unknown nucleotides: "+unknown);
+	Semaphore semaphore = new Semaphore(NUM_THREADS);
+	File[] listofFiles = new File("/Users/mitchnodzak/adenomasRelease/fasta").listFiles();
+	for (File file : listofFiles) {
+			semaphore.acquire();
+			NucleoCounterMulti scanFile = new NucleoCounterMulti(file, semaphore);
+			Worker work = new Worker(scanFile);
+			new Thread(work).start();
+	}
+	
+	int num = 0;
+	while(num < NUM_THREADS)
+	{
+		semaphore.acquire();
+		num++;
+	}
+	
+	System.out.println("Count of A: "+adenosine.get());
+	System.out.println("Count of T: "+thymine.get());
+	System.out.println("Count of C: "+cytosine.get());
+	System.out.println("Count of G: "+guanine.get());
+	System.out.println("Unknown nucleotides: "+unknown.get());
 	long stopTime = System.currentTimeMillis();
 	float elapsedTime = ((stopTime - startTime)/1000f);
      System.out.println("The multithreaded method took: "+elapsedTime+ " seconds to run.");
